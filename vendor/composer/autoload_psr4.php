469f9a7dfdb033f8ddb86ebaeb904598d911fa04
Luis <luis.d15@hotmail.com>
2015-05-04 20:22:38 -0600

    creo que agregue la configuracion para crear formularios
    as

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -11,6 +11,7 @@ return array(
     'Stringy\\' => array($vendorDir . '/danielstjules/stringy/src'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
     'League\\Flysystem\\' => array($vendorDir . '/league/flysystem/src'),
+    'Illuminate\\Html\\' => array($vendorDir . '/illuminate/html'),
     'Illuminate\\' => array($vendorDir . '/laravel/framework/src/Illuminate'),
     'ClassPreloader\\' => array($vendorDir . '/classpreloader/classpreloader/src'),
     'Barryvdh\\LaravelIdeHelper\\' => array($vendorDir . '/barryvdh/laravel-ide-helper/src'),
